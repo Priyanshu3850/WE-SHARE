7d9b8aa86dbd61271d6b0d97cf799cd3df5a20c5
priyanshu soni <priyanshu15kpl@gmail.com>
2023-03-03 00:53:25 +0530

    4th

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose")
 require('dotenv').config();
 
 
-mongoose.connect("mongodb://localhost:27017/file_storage",{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+mongoose.connect(MONGO_URL,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
           console.log("connection successful");
 
 }).catch((e)=>{
